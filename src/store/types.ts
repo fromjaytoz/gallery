c70f6132878cabe64b3f214b800753e242eabd5d
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-10-01 15:56:17 +0800

    define redux types

---
+++ src/store/types.ts
@@ -0,0 +1,8 @@
+export const SET_USER = "SET_USER";
+export const SIGN_OUT = "SIGN_OUT";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ERROR = "SET_ERROR";
+export const NEED_VERIFICATION = "NEED_VERIFICATION";
+export const SET_SUCCESS = "SET_SUCCESS";
+export const GET_IMAGES = "GET_IMAGES";
+export const DELETE_IMAGE = "DELETE IMAGE";
